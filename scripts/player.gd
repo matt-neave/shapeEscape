18c432450f63a619f46c282df6544f02d17f646e
Matt <matthewneaveis@hotmail.co.uk>
2024-08-18 10:27:07 +0100

    Bugfix: player collision layer was not set for scene

--- scripts/player.gd
+++ scripts/player.gd
@@ -19,11 +19,11 @@ func _ready():
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += gravity * 2 * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY * 2
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.

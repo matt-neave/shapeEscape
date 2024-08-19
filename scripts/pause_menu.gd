a7fa963e6abc06f0f4c31d361fc7f1c25dc17830
Jay Chung <jc2520@ic.ac.uk>
2024-08-19 18:40:23 +0100

    Added exit game option in pause menu

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -2,16 +2,22 @@ extends CanvasLayer
 
 @onready var resume = $CenterContainer/VBoxContainer/Resume
 @onready var back_to_level_map = $CenterContainer/VBoxContainer/BackToLevelMap
+@onready var exit_game = $CenterContainer/VBoxContainer/ExitGame
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_to_group("pause")
-	resume.pressed.connect(pause)
+	resume.pressed.connect(_pause)
 	back_to_level_map.pressed.connect(_back_to_level_map)
+	exit_game.pressed.connect(_exit_game)
 	
-func pause():
+func _pause():
 	get_tree().paused = !get_tree().paused
 	visible = !visible
+	
+func _exit_game() -> void:
+	SoundManager.play_sound(SoundManager.SOUNDS.BUTTON_CLICK)
+	get_tree().quit()
 
 func _back_to_level_map():
 	get_tree().paused = false
@@ -19,4 +25,4 @@ func _back_to_level_map():
 
 func _process(_delta):
 	if Input.is_action_just_pressed("pause"):
-		pause()
+		_pause()
